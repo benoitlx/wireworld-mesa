08cce355e209358ff8400d34419d0224597cfb7b
Benoit <benoitleroux41@gmail.com>
2024-03-16 12:34:39 +0100

    portrayal

---
+++ wireworld/portrayal.py
@@ -0,0 +1,18 @@
+def portayCell(cell):
+    """
+    This function is to be called each tick for each cells
+    to indicate how to draw the cell in the current state
+    """
+    if cell is None:
+        raise AssertionError
+    
+    return {
+        "Shape": "rect",
+        "w": 1,
+        "h": 1,
+        "Filled": "true",
+        "Layer": 0,
+        "x": cell.x,
+        "y": cell.y,
+        "Color": ["black", "orange", "blue", "white"][cell.state],
+    }
